f65b347d5a7875513f14feeff4ff421fbacff115
unknown <erictroost@msn.com>
2016-07-08 18:00:36 +0200

    Added rock item

---
+++ VertigoAssignment/Assets/Scripts/Items/Rock.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : Equipable
+{
+  // Use this for initialization
+  void Start()
+  {
+    // find audio source
+    m_audio = GetComponent<AudioSource>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  // will be called when the item is used
+  public override void ActivateItem()
+  {
+    base.ActivateItem();
+
+    // get forward
+    Vector3 forward = holdingHand.playerBody.transform.forward;
+    // release the rock from the hand
+    holdingHand.DropItem();
+
+    // add forward force to the rock
+    GetComponent<Rigidbody>().AddForce(forward * 400);
+
+    // play throwing sound
+    m_audio.Play();
+  }
+
+  AudioSource m_audio;
+}
